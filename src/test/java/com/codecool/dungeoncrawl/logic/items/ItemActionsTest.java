6a2749d9f445e41181e0f880ca90f62de5f0c7f5
moloch291 <danielkomjati1@gmail.com>
2021-09-06 14:01:59 +0200

    Create first test skeletons.

---
+++ src/test/java/com/codecool/dungeoncrawl/logic/items/ItemActionsTest.java
@@ -0,0 +1,46 @@
+package com.codecool.dungeoncrawl.logic.items;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemActionsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void searchForItemByType() {
+    }
+
+    @Test
+    void consumeFood() {
+    }
+
+    @Test
+    void consumePotion() {
+    }
+
+    @Test
+    void consumeAlcohol() {
+    }
+
+    @Test
+    void leaveBoat() {
+    }
+
+    @Test
+    void equipArmor() {
+    }
+
+    @Test
+    void equipWeapon() {
+    }
+}
---
+++ src/test/java/com/codecool/dungeoncrawl/logic/map/GameMapTest.java
@@ -0,0 +1,102 @@
+package com.codecool.dungeoncrawl.logic.map;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameMapTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getKraken() {
+    }
+
+    @Test
+    void getGhosts() {
+    }
+
+    @Test
+    void getPlayer() {
+    }
+
+    @Test
+    void getWidth() {
+    }
+
+    @Test
+    void getHeight() {
+    }
+
+    @Test
+    void getSkeletons() {
+    }
+
+    @Test
+    void getOrcs() {
+    }
+
+    @Test
+    void getUndeads() {
+    }
+
+    @Test
+    void getCells() {
+    }
+
+    @Test
+    void getCell() {
+    }
+
+    @Test
+    void getExit() {
+    }
+
+    @Test
+    void setPlayer() {
+    }
+
+    @Test
+    void addKraken() {
+    }
+
+    @Test
+    void addGhost() {
+    }
+
+    @Test
+    void addSkeleton() {
+    }
+
+    @Test
+    void addOrc() {
+    }
+
+    @Test
+    void addUndead() {
+    }
+
+    @Test
+    void removeSkeleton() {
+    }
+
+    @Test
+    void removeOrc() {
+    }
+
+    @Test
+    void removeUndead() {
+    }
+
+    @Test
+    void openDoor() {
+    }
+}
