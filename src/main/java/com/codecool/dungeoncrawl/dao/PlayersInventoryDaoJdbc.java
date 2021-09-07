686f183a319d0d3433df3baf60daf089b6a499da
janoscsomos <jstendec@gmail.com>
2021-09-07 17:51:49 +0200

    Add player inventory related classes and interfaces to dao/model

---
+++ src/main/java/com/codecool/dungeoncrawl/dao/PlayersInventoryDaoJdbc.java
@@ -0,0 +1,28 @@
+package com.codecool.dungeoncrawl.dao;
+
+import com.codecool.dungeoncrawl.model.PlayersInventory;
+
+import java.util.List;
+
+public class PlayersInventoryDaoJdbc implements PlayersInventoryDao{
+
+    @Override
+    public void add(PlayersInventory playersInventory) {
+
+    }
+
+    @Override
+    public void update(PlayersInventory playersInventory) {
+
+    }
+
+    @Override
+    public PlayersInventory get(int id) {
+        return null;
+    }
+
+    @Override
+    public List<PlayersInventory> getAll() {
+        return null;
+    }
+}
