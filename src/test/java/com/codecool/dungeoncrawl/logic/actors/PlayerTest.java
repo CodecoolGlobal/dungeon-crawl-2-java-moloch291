e46767bea5244a6b0c0164becfa02f7d67584a66
Sztomi8 <szilagyitamas94@gmail.com>
2021-09-07 11:57:06 +0200

    Implement the PlayerTest class

--- src/test/java/com/codecool/dungeoncrawl/logic/actors/PlayerTest.java
+++ src/test/java/com/codecool/dungeoncrawl/logic/actors/PlayerTest.java
@@ -1,98 +1,87 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
+import com.codecool.dungeoncrawl.logic.items.Alcohol;
+import com.codecool.dungeoncrawl.logic.items.Item;
+import com.codecool.dungeoncrawl.logic.items.ItemType;
+import com.codecool.dungeoncrawl.logic.map.CellType;
+import com.codecool.dungeoncrawl.logic.map.GameMap;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
 
-    @BeforeEach
-    void setUp() {
-    }
 
-    @AfterEach
-    void tearDown() {
+    @Test
+    void playerTileNameIsCorrect(){
+        GameMap gameMap = new GameMap(3, 3, CellType.FLOOR);
+        Player player = new Player(gameMap.getCell(1,1));
+        String correctName = "player";
+        String playerName = player.getTileName();
+        assertEquals(correctName,playerName);
     }
 
     @Test
-    void getHealth() {
+    void playerInventoryIsHashMap(){
+        GameMap gameMap = new GameMap(3, 3, CellType.FLOOR);
+        Player player = new Player(gameMap.getCell(1,1));
+        Map<Item,Integer> hashMap = new HashMap<>();
+        Map<Item,Integer> playerHashMap = player.getInventory();
+        assertEquals(hashMap,playerHashMap);
     }
 
     @Test
-    void getCell() {
+    void playerIsNotDrunkONStart(){
+        GameMap gameMap = new GameMap(3, 3, CellType.FLOOR);
+        Player player = new Player(gameMap.getCell(1,1));
+        assertFalse(player.isDrunk());
     }
 
     @Test
-    void getX() {
+    void setPlayerToDrunk(){
+        GameMap gameMap = new GameMap(3, 3, CellType.FLOOR);
+        Player player = new Player(gameMap.getCell(1,1));
+        player.setDrunk(true);
+        assertTrue(player.isDrunk());
     }
 
     @Test
-    void getY() {
+    void  addItemToInventory(){
+        GameMap gameMap = new GameMap(3, 3, CellType.FLOOR);
+        Item alcohol = new Alcohol("Alcohol",gameMap.getCell(1,1), ItemType.ALCOHOL);
+        Player player = new Player(gameMap.getCell(1,1));
+        player.addToInventory(alcohol,1);
+        assertTrue(player.hasItem(ItemType.ALCOHOL));
     }
 
     @Test
-    void getAttack() {
+    void removeFromInventory(){
+        GameMap gameMap = new GameMap(3, 3, CellType.FLOOR);
+        Item alcohol = new Alcohol("Alcohol",gameMap.getCell(1,1), ItemType.ALCOHOL);
+        Player player = new Player(gameMap.getCell(1,1));
+        player.addToInventory(alcohol,1);
+        player.removeFromInventory(alcohol);
+        assertFalse(player.hasItem(ItemType.ALCOHOL));
     }
 
     @Test
-    void getDefense() {
+    void moveToNextCell(){
+        GameMap gameMap = new GameMap(3, 3, CellType.FLOOR);
+        Player player = new Player(gameMap.getCell(1,1));
+        player.move(0,1);
+        assertEquals(player.getY(),2);
     }
 
     @Test
-    void setCell() {
+    void moveThrowsWithBadInput(){
+        GameMap gameMap = new GameMap(3, 3, CellType.FLOOR);
+        Player player = new Player(gameMap.getCell(1,1));
+        assertThrows(IndexOutOfBoundsException.class,()->player.move(0,99));
     }
 
-    @Test
-    void setHealth() {
-    }
-
-    @Test
-    void setAttack() {
-    }
-
-    @Test
-    void setDefense() {
-    }
-
-    @Test
-    void move() {
-    }
-
-    @Test
-    void getTileName() {
-    }
-
-    @Test
-    void getInventory() {
-    }
-
-    @Test
-    void isDrunk() {
-    }
-
-    @Test
-    void setDrunk() {
-    }
-
-    @Test
-    void addToInventory() {
-    }
-
-    @Test
-    void removeFromInventory() {
-    }
-
-    @Test
-    void hasItem() {
-    }
-
-    @Test
-    void testMove() {
-    }
-
-    @Test
-    void monsterMove() {
-    }
 }
