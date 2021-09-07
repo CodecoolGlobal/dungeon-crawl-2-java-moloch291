80a0e85723f3930a2bfd152b0ccc0ab6499cbfbd
moloch291 <danielkomjati1@gmail.com>
2021-09-07 16:59:58 +0200

    implement searchForItemByType test cases

--- src/test/java/com/codecool/dungeoncrawl/logic/items/ItemActionsTest.java
+++ src/test/java/com/codecool/dungeoncrawl/logic/items/ItemActionsTest.java
@@ -1,22 +1,90 @@
 package com.codecool.dungeoncrawl.logic.items;
 
+import com.codecool.dungeoncrawl.logic.actors.Player;
+import com.codecool.dungeoncrawl.logic.map.CellType;
+import com.codecool.dungeoncrawl.logic.map.GameMap;
+import com.codecool.dungeoncrawl.logic.util.StringFactory;
 import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ItemActionsTest {
 
+    private GameMap gameMap;
+    private Player player;
+    private ItemActions itemActions;
+
     @BeforeAll
     static void initTest() {
-        System.out.println("Testing ItemActions...");
+        System.out.println("Testing ItemActions methods...");
     }
 
     @BeforeEach
     void setUp() {
+        gameMap = new GameMap(3, 3, CellType.FLOOR);
+        player = new Player(gameMap.getCell(0, 0));
+        gameMap.setPlayer(player);
+        itemActions = new ItemActions();
     }
 
     @Test
-    void searchForItemByType() {
+    void searchForItemByTypeWithSimilarTypes() {
+
+        Food bread = new Food(
+                StringFactory.BREAD_CAP.message,
+                gameMap.getCell(0, 0),
+                FoodType.BREAD
+                );
+        Food cheese = new Food(
+                StringFactory.CHEESE_CAP.message,
+                gameMap.getCell(0, 0),
+                FoodType.CHEESE
+        );
+
+        player.addToInventory(bread, 1);
+        player.addToInventory(cheese, 1);
+
+        assertEquals(cheese, itemActions.searchForItemByType(gameMap, ItemType.FOOD));
+    }
+
+    @Test
+    void searchForItemByTypeWithDifferentTypes() {
+
+        Weapon sword = new Weapon(
+                StringFactory.SWORD_CAP.message,
+                gameMap.getCell(0, 0),
+                WeaponType.SWORD
+        );
+        Food cheese = new Food(
+                StringFactory.CHEESE_CAP.message,
+                gameMap.getCell(0, 0),
+                FoodType.CHEESE
+        );
+
+        player.addToInventory(sword, 1);
+        player.addToInventory(cheese, 1);
+
+        assertEquals(cheese, itemActions.searchForItemByType(gameMap, ItemType.FOOD));
+    }
+
+    @Test
+    void searchForItemByTypeWithNoneExistentType() {
+
+        Weapon sword = new Weapon(
+                StringFactory.SWORD_CAP.message,
+                gameMap.getCell(0, 0),
+                WeaponType.SWORD
+        );
+        Food cheese = new Food(
+                StringFactory.CHEESE_CAP.message,
+                gameMap.getCell(0, 0),
+                FoodType.CHEESE
+        );
+
+        player.addToInventory(sword, 1);
+        player.addToInventory(cheese, 1);
+
+        assertNull(itemActions.searchForItemByType(gameMap, ItemType.KEY));
     }
 
     @Test
