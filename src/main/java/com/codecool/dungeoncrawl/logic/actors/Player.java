2a5610368510156edb9c083b66ba7382d439d5a2
Sztomi8 <szilagyitamas94@gmail.com>
2021-09-07 17:17:30 +0200

    refactor monsterMove method of Player class

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -61,6 +61,6 @@ public class Player extends Actor {
 
     @Override
     public void monsterMove(Cell playerCell) {
-        throw new RuntimeException("Play monsterMove not implemented");
+        System.out.println("Play monsterMove not implemented");
     }
 }
