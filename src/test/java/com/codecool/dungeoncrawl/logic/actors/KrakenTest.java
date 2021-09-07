083bad62c86e468ef56bb9415bb695f9c82d617f
Sztomi8 <szilagyitamas94@gmail.com>
2021-09-07 14:42:46 +0200

    refactor the KrakenTest class

--- src/test/java/com/codecool/dungeoncrawl/logic/actors/KrakenTest.java
+++ src/test/java/com/codecool/dungeoncrawl/logic/actors/KrakenTest.java
@@ -3,65 +3,65 @@ package com.codecool.dungeoncrawl.logic.actors;
 import com.codecool.dungeoncrawl.logic.map.Cell;
 import com.codecool.dungeoncrawl.logic.map.CellType;
 import com.codecool.dungeoncrawl.logic.map.GameMap;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class KrakenTest {
 
+    GameMap gameMapWithFloorCells;
+    GameMap gameMapWithWaterCells;
+    Kraken krakenInTheFloor;
+    Kraken krakenIntTheWater;
+
+    @BeforeEach
+    void setFields(){
+        gameMapWithFloorCells = new GameMap(3, 3, CellType.FLOOR);
+        gameMapWithWaterCells = new GameMap(3, 3, CellType.WATER);
+        krakenInTheFloor = new Kraken(gameMapWithFloorCells.getCell(1,1),gameMapWithFloorCells);
+        krakenIntTheWater = new Kraken(gameMapWithWaterCells.getCell(1,1),gameMapWithWaterCells);
+    }
+
     @Test
     void dontCollectFloor(){
-        GameMap gameMap = new GameMap(3, 3, CellType.FLOOR);
-        Kraken kraken = new Kraken(gameMap.getCell(1,1),gameMap);
-        assertEquals(kraken.getWaterCells().size(),0);
+        assertEquals(krakenInTheFloor.getWaterCells().size(),0);
     }
 
     @Test
     void collectWater(){
-        GameMap gameMap = new GameMap(3, 3, CellType.WATER);
-        Kraken kraken = new Kraken(gameMap.getCell(1,1),gameMap);
-        assertNotEquals(kraken.getWaterCells().size(),0);
+        assertNotEquals(krakenIntTheWater.getWaterCells().size(),0);
     }
 
     @Test
-    void playerTileNameIsCorrect(){
-        GameMap gameMap = new GameMap(3, 3, CellType.FLOOR);
-        Kraken kraken = new Kraken(gameMap.getCell(1,1),gameMap);
+    void krakenTileNameIsCorrect(){
         String correctName = "kraken";
-        String playerName = kraken.getTileName();
+        String playerName = krakenInTheFloor.getTileName();
         assertEquals(correctName,playerName);
     }
 
     @Test
     void moveThrowsWithBadInput(){
-        GameMap gameMap = new GameMap(3, 3, CellType.FLOOR);
-        Kraken kraken = new Kraken(gameMap.getCell(1,1),gameMap);
-        assertThrows(IndexOutOfBoundsException.class,()->kraken.move(0,99));
+        assertThrows(IndexOutOfBoundsException.class,()->krakenInTheFloor.move(0,99));
     }
 
     @Test
     void notMovingIntoWater(){
-        GameMap gameMap = new GameMap(3, 3, CellType.FLOOR);
-        Kraken kraken = new Kraken(gameMap.getCell(1,1),gameMap);
-        kraken.move(2,2);
-        assertNotEquals(kraken.getCell(),gameMap.getCell(2,2));
+        krakenInTheFloor.move(2,2);
+        assertNotEquals(krakenInTheFloor.getCell(),gameMapWithFloorCells.getCell(2,2));
     }
 
     @Test
     void MovingIntoWater(){
-        GameMap gameMap = new GameMap(3, 3, CellType.WATER);
-        Kraken kraken = new Kraken(gameMap.getCell(1,1),gameMap);
-        kraken.move(2,2);
-        assertEquals(kraken.getCell(),gameMap.getCell(2,2));
+        krakenIntTheWater.move(2,2);
+        assertEquals(krakenIntTheWater.getCell(),gameMapWithWaterCells.getCell(2,2));
     }
 
     @Test
     void MonsterMoveMethodWorks(){
-        GameMap gameMap = new GameMap(3, 3, CellType.WATER);
-        Kraken kraken = new Kraken(gameMap.getCell(1,1),gameMap);
-        Cell originalKrakenCell = kraken.getCell();
-        kraken.monsterMove(originalKrakenCell);
-        assertNotEquals(kraken.getCell(),originalKrakenCell);
+        Cell originalKrakenCell = krakenIntTheWater.getCell();
+        krakenIntTheWater.monsterMove(originalKrakenCell);
+        assertNotEquals(krakenIntTheWater.getCell(),originalKrakenCell);
     }
 
 
--- src/test/java/com/codecool/dungeoncrawl/logic/actors/OrcTest.java
+++ src/test/java/com/codecool/dungeoncrawl/logic/actors/OrcTest.java
@@ -1,6 +1,50 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
+import com.codecool.dungeoncrawl.logic.map.CellType;
+import com.codecool.dungeoncrawl.logic.map.GameMap;
+import com.codecool.dungeoncrawl.logic.util.Direction;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class OrcTest {
-  
+
+    GameMap gameMap;
+    Orc orc;
+
+    @BeforeEach
+    void setFields(){
+        gameMap = new GameMap(3, 3, CellType.FLOOR);
+        orc = new Orc(gameMap.getCell(1,1));
+    }
+
+    @Test
+    void moveThrowsWithBadInput(){
+        assertThrows(IndexOutOfBoundsException.class,()->orc.move(0,99));
+    }
+
+    @Test
+    void orcTileNameIsCorrect(){
+        String correctName = "orc";
+        String playerName = orc.getTileName();
+        assertEquals(correctName,playerName);
+    }
+
+    @Test
+    void moveToWest(){
+        Direction originalDirection = orc.getDirection();
+        orc.monsterMove(orc.getCell());
+        assertNotEquals(originalDirection,orc.getDirection());
+    }
+    @Test
+    void moveToSouth(){
+        orc.setDirection(Direction.WEST);
+        Direction originalDirection = orc.getDirection();
+        orc.monsterMove(orc.getCell());
+        assertNotEquals(originalDirection,orc.getDirection());
+    }
+
+
+
 }
