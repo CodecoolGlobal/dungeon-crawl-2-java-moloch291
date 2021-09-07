3fb0fc445acfdbc48284356e0aa4b16c76695b8c
Sztomi8 <szilagyitamas94@gmail.com>
2021-09-07 17:37:26 +0200

    implement the UndeadTest class

---
+++ src/test/java/com/codecool/dungeoncrawl/logic/actors/UndeadTest.java
@@ -0,0 +1,35 @@
+package com.codecool.dungeoncrawl.logic.actors;
+
+import com.codecool.dungeoncrawl.logic.map.Cell;
+import com.codecool.dungeoncrawl.logic.map.CellType;
+import com.codecool.dungeoncrawl.logic.map.GameMap;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UndeadTest {
+    GameMap gameMap;
+    Undead undead;
+
+    @BeforeEach
+    void setFields(){
+        gameMap = new GameMap(4, 4, CellType.FLOOR);
+        undead = new Undead(gameMap.getCell(1,1));
+    }
+
+    @Test
+    void undeadTileNameIsCorrect(){
+        String correctName = "undead";
+        String skeletonName = undead.getTileName();
+        assertEquals(correctName,skeletonName);
+    }
+
+    @Test
+    void undeadDoesNotMoving(){
+        Cell originalCell = undead.getCell();
+        undead.monsterMove(undead.getCell());
+        assertEquals(originalCell,undead.getCell());
+    }
+
+}
